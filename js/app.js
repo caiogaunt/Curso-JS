e90a8a8c0e0091a15c355a190f9a87b9e014e344
Caio Oliveira <Caiogaunt@gmail.com>
2023-11-13 16:15:57 +0100

    dd

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
-console.log(
-    document.getElementsByClassName("carro")
-)
+var paragrafo = document.getElementById("p1")
+paragrafo.innerHTML = "Novo conteúdo"
+
+paragrafo.style.color = "red"
+paragrafo.style.backgroundColor = "#333"
+paragrafo.style.height = "400px"
